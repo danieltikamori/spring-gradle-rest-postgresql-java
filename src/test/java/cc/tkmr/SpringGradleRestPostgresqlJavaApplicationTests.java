16aba492a99419d7311c6048ad119210773578b1
danieltikamori <tikamori.itiro@gmail.com>
2024-04-16 06:12:18 +0900

    Initial commit

---
+++ src/test/java/cc/tkmr/SpringGradleRestPostgresqlJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.tkmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleRestPostgresqlJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
